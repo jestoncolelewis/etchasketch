84f53fc76757b13fd6b40440156d532aca79dd2c
Jeston Lewis <jeston.lewis@protonmail.com>
2022-12-27 16:32:16 -0700

    started dynamic input for grid

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const container = document.querySelector('#container');
+let gridSize = 16;
+const MAX = 100;
 
-// 16 x 16 grid
-for (let i = 1; i < 257; i++) {
+// button for size
+
+
+// grid
+let divSize = 960/gridSize;
+for (let i = 1; i < gridSize * gridSize + 1; i++) {
     const div = document.createElement('div');
     div.setAttribute('id', i);
+    div.style.height = divSize; // need to push to html
+    div.style.width = divSize; // need to push to html
     container.append(div);
 }
 
+// hover
 const divs = container.querySelectorAll('div');
 divs.forEach((div) => {
     div.addEventListener('mouseenter', () => {
-        div.setAttribute('style', 'background-color: red');
+        div.setAttribute('style', 'background-color: red;');
     });
 });
