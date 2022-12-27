18890c0b8883b79bf8e7dcdc111859c29f4ea808
Jeston Lewis <jeston.lewis@protonmail.com>
2022-12-27 16:21:03 -0700

    built hover

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ for (let i = 1; i < 257; i++) {
     div.setAttribute('id', i);
     container.append(div);
 }
+
+const divs = container.querySelectorAll('div');
+divs.forEach((div) => {
+    div.addEventListener('mouseenter', () => {
+        div.setAttribute('style', 'background-color: red');
+    });
+});
