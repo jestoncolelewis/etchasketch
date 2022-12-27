36500e6fa2018d56b214bb2ea58d1a8fb4afc521
Jeston Lewis <jeston.lewis@protonmail.com>
2022-12-27 16:01:54 -0700

    created div loop

---
+++ app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+// 16 x 16 grid
+for (let i = 1; i < 17; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('id', i);
+    container.append(div);
+}
