ce854f4577d9e9fc44858e0fc226d37ba5564a3a
Jeston Lewis <jeston.lewis@protonmail.com>
2022-12-27 17:23:00 -0700

    built function for button resize

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const container = document.querySelector('#container');
 let gridSize = 16;
 const MAX = 100;
 
-// button for size
-
-
 // grid
 let divSize = 960/gridSize;
-for (let i = 1; i < gridSize * gridSize + 1; i++) {
-    const div = document.createElement('div');
-    div.style.height = divSize; // need to push to html
-    div.style.width = divSize; // need to push to html
-    container.append(div);
-}
-
-// hover
-const divs = container.querySelectorAll('div');
-divs.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-        div.setAttribute('style', 'background-color: red;');
+function grid(gridSize) {
+    for (let i = 1; i < gridSize * gridSize + 1; i++) {
+        const div = document.createElement('div');
+        div.style.height = (divSize + 'px'); // need to push to html
+        div.style.width = (divSize + 'px'); // need to push to html
+        container.append(div);
+    }
+    // hover
+    const divs = container.querySelectorAll('div');
+    divs.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = 'red';
+        });
     });
+};
+
+// button for size
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    gridSize = parseInt(prompt('How big of a grid?'));
+    grid(gridSize);
 });
+
+grid(gridSize);
